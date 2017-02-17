08cc7a712c7770e8dba427dcbc7025d56256a9a6
Carlson Santana Cruz <carlsonsantana@gmail.com>
2017-02-16 21:32:38 -0300

    Add brasilian portuguese language

---
+++ _locales/pt_BR/js/skippers.js
@@ -0,0 +1,7 @@
+var hatemile_configuration_skippers;
+
+hatemile_configuration_skippers = [
+	{'selector': 'main,[role=main]', 'description': 'Ir para o conteúdo principal', 'shortcut': '1'}
+	, {'selector': '#container-shortcuts', 'description': 'Ir para a lista de atalhos', 'shortcut': '9'}
+	, {'selector': '#container-heading', 'description': 'Ir para o sumário', 'shortcut': '0'}
+];
